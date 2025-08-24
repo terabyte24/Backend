39368a31b728f4ed6dc7acf84ba32e829e72dce8
terabyte24 <mark1120241@gmail.com>
2025-08-24 18:33:31 +0530

    db established

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 
 // require('dotenv').config({path: './env'})
-import dotenv form "dotenv" 
+import dotenv from "dotenv" 
 
-import connectDB from "./db"
+import connectDB from "./db/index.js";
 
 dotenv.config(
   {
     path: './env'
   }
 )
+
+
 connectDB()
 
 
