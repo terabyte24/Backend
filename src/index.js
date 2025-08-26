fc404f414940d13c11640da5c447047d9a7efe60
terabyte24 <mark1120241@gmail.com>
2025-08-26 13:28:16 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -12,25 +12,19 @@ dotenv.config(
 
 
 connectDB()
+ 
+.then(()=>{
+app.listen(process.env.PORT || 8000 ,()=>{
+  console.log(` Server is running at port : ${process.env.PORT}`);
+} )
+
+})
 
 
 
+.catch((err) => {
+  console.log("MONGO db connection failed !!! " , err );
+})
 
-// import express from "express";
-// const app = express();
 
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MOGODB_URI}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("ERR:", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on pirt${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("ERROR: ", error);
-//     throw err;
-//   }
-// })();
+
